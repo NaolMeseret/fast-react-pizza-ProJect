a3880b2a8bb518812c62cce030dbe363fd8d7993
Naol <naolmes15062015@gmail.com>
2025-12-04 01:24:06 +0300

    pushed

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,15 @@
+import CartOverview from "../features/cart/CartOverview"
 import Header from "./Header"
 
 function AppLayout() {
   return (
     <div>
       <Header />
+
+      <main>
+        <h1>Content</h1>
+      </main>
+      <CartOverview />
     </div>
   )
 }
